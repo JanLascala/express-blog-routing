2cfd384812224a4dd91c086a41ba1b8addf83d55
JanLascala <lascala.jan@gmail.com>
2025-03-13 14:43:31 +0100

    route complete

--- routers/posts.js
+++ routers/posts.js
@@ -2,7 +2,35 @@ const express = require('express');
 const router = express.Router();
 
 //Index
-
 router.get('/', (req, res) => {
     res.send(`List of posts `);
 });
+
+// show
+router.get('/:id', (req, res) => {
+    res.send(`posts details ${req.params.id}`)
+});
+
+// store
+router.post('/', (req, res) => {
+    res.send(`store new post`)
+});
+
+//update 
+
+router.put('/:id', (req, res) => {
+    res.send(`updated posts ${req.params.id}`)
+});
+
+//patch or modify
+
+router.patch('/:id', (req, res) => {
+    res.send(`patch posts ${req.params.id}`)
+});
+
+// delete 
+router.delete('/:id', (res, req) => {
+    res.send(`Delete posts ${req.params.id}`)
+});
+
+module.exports = router
